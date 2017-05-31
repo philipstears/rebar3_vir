f43513c3bf7cd47c59a1a3a8576795b1d5af1a17
Rob Ashton <robashton@nuc.local>
2017-05-31 10:25:07 +0100

    Initial commit..

---
+++ src/vir_prv.erl
@@ -0,0 +1,37 @@
+-module(vir_prv).
+
+-behaviour(provider).
+
+-export([init/1, do/1, format_error/1]).
+
+-define(PROVIDER, vir).
+-define(DEPS, [release]).
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+            {name, ?PROVIDER},          % The 'user friendly' name of the task
+            {module, ?MODULE},          % The module implementation of the task
+            {bare, true},               % The task can be run by the user, always true
+            {deps, ?DEPS},              % The list of dependencies
+            {example, "rebar vir..."},  % How to use the plugin
+            {opts, []}                  % list of options understood by the plugin
+            {short_desc, "Drop in replacement for vir"},
+            {desc, ""}
+    ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+  %% For now, just execute the bundled shell script..
+  os:cmd("priv/vir.sh -fd")
+  {ok, State}.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
+
